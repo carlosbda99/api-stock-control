06dde9be200e836b35699c0d7cff812be1e916c8
Carlos Henrique <carlosbda99@hotmail.com>
2021-07-20 12:37:38 -0300

    Heroku

--- src/index.ts
+++ src/index.ts
@@ -20,7 +20,8 @@ connection
 .catch( (error: Error): void => console.log(error));
 
 const routers = [utilsRouter, productRouter, providerRouter, categoryRouter]
-const PORT: string | undefined = process.env.API_PORT
+// const PORT: string | undefined = process.env.API_PORT
+const PORT: number = 443
 
 const app = express()
 app.use(cors())
